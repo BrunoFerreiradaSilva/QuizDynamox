c08a61a91a3df9214116b9e9589c147817049c6a
BrunoFerreiradaSilva <31040011+BrunoFerreiradaSilva@users.noreply.github.com>
2023-07-23 08:57:02 -0300

    incluido logo e nome do app na tela de start

--- app/src/main/java/com/example/quizdynamox/ui/screens/quizStart/StartQuizScreen.kt
+++ app/src/main/java/com/example/quizdynamox/ui/screens/quizStart/StartQuizScreen.kt
@@ -1,7 +1,11 @@
 package com.example.quizdynamox.ui.screens.quizStart
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -18,9 +22,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.quizdynamox.R
 import com.example.quizdynamox.ui.components.ButtonComponent
@@ -38,10 +46,43 @@ fun HomeScreen(onQuizScreen: (String) -> Unit) {
             .fillMaxSize()
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
     ) {
+        Image(
+            modifier = Modifier.padding(top = 10.dp),
+            painter = painterResource(id = R.drawable.speech_bubble),
+            contentDescription = stringResource(id = R.string.content_image_logo),
 
-        NameForGame(nameUser, state.fieldValid)
+            )
+
+        Text(
+            text = stringResource(id = R.string.apresentation_app),
+            fontSize = 26.sp,
+            fontWeight = FontWeight.ExtraBold
+        )
+
+
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 100.dp),
+            value = nameUser.value,
+            onValueChange = { nameUser.value = it },
+            label = {
+                Text(
+                    text = stringResource(id = R.string.name_hint),
+                    color = MaterialTheme.colorScheme.onTertiary
+                )
+            },
+            shape = ShapeDefaults.Medium,
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                textColor = MaterialTheme.colorScheme.onTertiary,
+                focusedBorderColor = MaterialTheme.colorScheme.primary,
+                unfocusedBorderColor = MaterialTheme.colorScheme.primary
+            ),
+            isError = state.fieldValid,
+            maxLines = 1,
+            singleLine = true
+        )
 
 
         state.showErrorName?.let {
@@ -62,29 +103,3 @@ fun HomeScreen(onQuizScreen: (String) -> Unit) {
     }
 }
 
-@Composable
-private fun NameForGame(
-    nameUser: MutableState<TextFieldValue>,
-    isError: Boolean
-) {
-    OutlinedTextField(
-        modifier = Modifier.fillMaxWidth(),
-        value = nameUser.value,
-        onValueChange = { nameUser.value = it },
-        label = {
-            Text(
-                text = stringResource(id = R.string.name_hint),
-                color = MaterialTheme.colorScheme.onTertiary
-            )
-        },
-        shape = ShapeDefaults.Medium,
-        colors = TextFieldDefaults.outlinedTextFieldColors(
-            textColor = MaterialTheme.colorScheme.onTertiary,
-            focusedBorderColor = MaterialTheme.colorScheme.primary,
-            unfocusedBorderColor = MaterialTheme.colorScheme.primary
-        ),
-        isError = isError,
-        maxLines = 1,
-        singleLine = true
-    )
-}
